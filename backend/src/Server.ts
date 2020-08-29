53a48140f91d86a5643e8284ec64d79c619f2b3c
Noel Schwabenland <noel@schwabenland.ch>
2020-08-29 23:09:45 +0200

    initial

---
+++ backend/src/Server.ts
@@ -0,0 +1,49 @@
+import logger from '@shared/Logger';
+import cookieParser from 'cookie-parser';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import helmet from 'helmet';
+import { BAD_REQUEST } from 'http-status-codes';
+import morgan from 'morgan';
+import path from 'path';
+
+import BaseRouter from './routes';
+
+
+// Init express
+const app = express();
+
+
+
+/************************************************************************************
+ *                              Set basic express settings
+ ***********************************************************************************/
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// Show routes called in console during development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Security
+if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+}
+
+// Add APIs
+app.use('/api', BaseRouter);
+
+// Print API errors
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message, err);
+    return res.status(BAD_REQUEST).json({
+        error: err.message,
+    });
+});
+
+
+// Export express instance
+export default app;

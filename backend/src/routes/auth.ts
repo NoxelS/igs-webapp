7a647f54d1d0f1d9a3203ea31195789f40483723
Noel Schwabenland <noel@schwabenland.ch>
2020-09-03 16:15:07 +0200

    password recovery infrastructure

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,5 +1,7 @@
 import { isLoggedIn } from '@shared/passport.utils';
+import { sendEmail } from '@shared/utils';
 import { hash } from 'bcrypt';
+import { randomBytes } from 'crypto';
 import { NextFunction, Request, Response, Router } from 'express';
 import passport from 'passport';
 import { connection } from 'src/config/database';
@@ -52,5 +54,36 @@ router.post('/create_user', isLoggedIn, async (req: Request, res: Response, next
         }
     });
 });
+
+/** TODO: Get id by email */
+router.post('/recovery', isLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
+    const recoveryKey =  randomBytes(20).toString('hex');
+    connection.query('INSERT INTO recovery (`user_id`, `key`, `expires`) VALUES (?,?,?);', [(req as any).session.passport.user.user_id, recoveryKey, (new Date().getTime() + 1000 * 60 * 60 * 24)], async (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            await sendEmail('Recovery <test@noel-s.ch>', 'noel@schwabenland.ch', 'Recovery!', `<b>Recovery key = ${recoveryKey}</b></br><a href="localhost:3000/recover_password">`)
+            res.json({user: (req as any).session.passport.user.user_id})
+        }
+    })
+});
+
+router.post('/recover_password',  async (req: Request, res: Response, next: NextFunction) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const recoveryKey = req.body.recoveryKey;
+
+    connection.query('SELECT * FROM igs.recovery WHERE (`key` = ?);', recoveryKey, (err, result) => {
+        if(err) {
+            res.json({success: false});
+        } else {
+            if((result[0].expires - new Date().getTime()) > 0) {
+                res.json({success: true})
+            } else {
+                res.json({success: false, reason: 'expired'});
+            }
+        }
+    })
+})
 
 export default router;
--- backend/src/shared/utils.ts
+++ backend/src/shared/utils.ts
@@ -1,7 +1,31 @@
 import { NextFunction, Request, Response } from 'express';
+import { createTransport } from 'nodemailer';
 
 
 export function setLocals(req: Request, res: Response, next: NextFunction) {
     res.locals.loggedIn = req.isAuthenticated();
     next();
+}
+
+export async function sendEmail(from: string, to: string, subject: string, html: string, callback?: (info: any) => void) {
+    const transporter = createTransport({
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        secure: !!process.env.MAIL_SECURE,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASSWORD
+        }
+    });
+
+    const info = await transporter.sendMail({
+        from: `"IGS" <${process.env.MAIL_USER}>`,
+        to,
+        subject,
+        html
+    });
+
+    if (typeof callback === 'function') {
+        callback(info);
+    }
 }

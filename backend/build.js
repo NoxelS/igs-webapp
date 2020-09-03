34371c60947b4a03ddab06fd64f728a6ef8cfa65
Noel Schwabenland <noel@schwabenland.ch>
2020-09-03 12:23:03 +0200

    jenkins fix

--- backend/build.js
+++ backend/build.js
@@ -1,20 +1,17 @@
 const fs = require('fs-extra');
 const childProcess = require('child_process');
 
+// Remove current build
+fs.removeSync('./dist/');
 
-try {
-    // Remove current build
-    fs.removeSync('./dist/');
-    // Copy front-end files
-    fs.copySync('./src/public', './dist/public');
-    fs.copySync('./src/views', './dist/views');
-    // Transpile the typescript files
-    const proc = childProcess.exec('tsc --build tsconfig.prod.json');
-    proc.on('close', (code) => {
-        if (code !== 0) {
-            throw Error("Build failed")
-        }
-    })
-} catch (err) {
-    console.log(err);
-}
+// Copy front-end files
+fs.copySync('./src/public', './dist/public');
+fs.copySync('./src/views', './dist/views');
+
+// Transpile the typescript files
+const proc = childProcess.exec('tsc --build tsconfig.prod.json');
+proc.on('close', code => {
+    if (code !== 0) {
+        throw Error('Build failed');
+    }
+});

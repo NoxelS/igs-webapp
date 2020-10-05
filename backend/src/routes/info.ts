4803d39c8147491d662eacfceb8860f3dea61192
Noel Schwabenland <noel@schwabenland.ch>
2020-10-05 23:02:42 +0200

    info update

--- backend/src/routes/info.ts
+++ backend/src/routes/info.ts
@@ -1,6 +1,9 @@
 import { connection } from '@configs/database';
 import { isLoggedIn } from '@shared/passport.utils';
 import { Request, Response, Router } from 'express';
+import { readFileSync } from 'fs';
+import { verify } from 'jsonwebtoken';
+import { join } from 'path';
 import { ErrorResponse, IgsResponse } from 'src/models/response.model';
 import { User } from 'src/models/user.model';
 
@@ -9,12 +12,20 @@ import { User } from 'src/models/user.model';
 const router = Router();
 
 router.post('/user', isLoggedIn(), async (req: Request, res: Response) => {
-    const userId = (req as any).session.passport.user.user_id;
-    connection.query('SELECT * FROM igs.users WHERE (id=?);', [userId], (err, results) => {
+    const PRIV_KEY = readFileSync(join(__dirname, '../keys/id_rsa_priv.pem'));
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && (authHeader.split(' ')[1] as any);
+    verify(token, PRIV_KEY, { algorithms: ['RS256'] }, (err, subPrperties) => {
         if (err) {
             res.json(new ErrorResponse(err.message));
         } else {
-            res.json(new IgsResponse<User>(new User(results[0].username, results[0].email, results[0].id)));
+            connection.query('SELECT * FROM igs.users WHERE (id=?);', [(subPrperties as any).sub], (err, results) => {
+                if (err) {
+                    res.json(new ErrorResponse(err.message));
+                } else {
+                    res.json(new IgsResponse<User>(new User(results[0].username, results[0].email, results[0].id)));
+                }
+            });
         }
     });
 });

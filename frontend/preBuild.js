da9b537378b306d67f065fb43efe4c429b42b860
Noel Schwabenland <noel@schwabenland.ch>
2020-10-07 08:07:12 +0200

    frontend connection fix

--- frontend/preBuild.js
+++ frontend/preBuild.js
@@ -2,15 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 function copyFolderSync(from, to) {
-  fs.mkdirSync(to);
-  fs.readdirSync(from).forEach(element => {
-      if (fs.lstatSync(path.join(from, element)).isFile()) {
-          fs.copyFileSync(path.join(from, element), path.join(to, element));
-      } else {
-          copyFolderSync(path.join(from, element), path.join(to, element));
-      }
-  });
+    try {
+        fs.mkdirSync(to);
+    } catch (error) {}
+    fs.readdirSync(from).forEach(element => {
+        if (fs.lstatSync(path.join(from, element)).isFile()) {
+            fs.copyFileSync(path.join(from, element), path.join(to, element));
+        } else {
+            copyFolderSync(path.join(from, element), path.join(to, element));
+        }
+    });
 }
 
-fs.rmdirSync('./src/app/backend-datatypes', {recursive: true});
-copyFolderSync('../backend/src/models','./src/app/backend-datatypes')
+fs.rmdirSync('./src/app/backend-datatypes', { recursive: true });
+copyFolderSync('../backend/src/models', './src/app/backend-datatypes');
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:3000/api'
+  backendUrl: 'http://igs.api.noel-s.ch/api'
 };
 
 /*

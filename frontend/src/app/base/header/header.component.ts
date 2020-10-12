07192b885429e8a52de81178fa78380748bb0e7a
Noel Schwabenland <noel@schwabenland.ch>
2020-10-12 16:56:25 +0200

    header

--- frontend/src/app/base/header/header.component.ts
+++ frontend/src/app/base/header/header.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
-  selector: 'igs-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'igs-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }

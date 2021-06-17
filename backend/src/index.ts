4e4a9d16a0e740dd8e0ff87f6069fd4de47b3cba
Noel Schwabenland <noel@schwabenland.ch>
2021-06-17 23:24:48 +0200

    debug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,5 +6,5 @@ import app from './server';
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
-    logger.info(`Server is starting on port ${port}`);
+    logger.info(`Server is starting on port ${port} in ${process.env.NODE_ENV} mode.`);
 });

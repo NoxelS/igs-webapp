4f4d24a8d9b722fbadb71fcbec22756d5f505368
Noel Schwabenland <noel@schwabenland.ch>
2021-06-18 15:03:40 +0200

    regionalgruppen enum

---
+++ backend/src/shared/groups.ts
@@ -0,0 +1,8 @@
+export enum Regionalgruppe {
+    koblenz = 'Koblenz Nord/Ost und Koblenz Süd/West',
+    neustadtNord = 'Neustadt/Nord',
+    neustadtWest = 'Neustadt/West',
+    neustadtMitte = 'Neustadt/Mitte',
+    neustadtSüd = 'Neustadt/Süd',
+    trier = 'Trier'
+}

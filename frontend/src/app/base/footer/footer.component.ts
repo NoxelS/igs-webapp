86755fed0fd32a5bde909f849a3c21fb6505fe26
Noel Schwabenland <noel@schwabenland.ch>
2021-06-18 19:46:43 +0200

    footer links

--- frontend/src/app/base/footer/footer.component.ts
+++ frontend/src/app/base/footer/footer.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Subscription } from 'rxjs';
+import { routePaths } from 'src/app/shared/routes.const';
+
 import { Article } from '../../backend-datatypes/article.model';
 import { ArticleService } from '../../services/items/article.service';
 
@@ -11,12 +14,25 @@ import { ArticleService } from '../../services/items/article.service';
 })
 export class FooterComponent implements OnInit {
     articleList: Article[] = [];
+    subscription: Subscription[] = [];
+
+    readonly routes = routePaths;
 
     constructor(private readonly articleService: ArticleService) {}
 
     ngOnInit(): void {
-        this.articleService.articles.subscribe(articles => {
-            this.articleList = articles;
-        });
+        this.subscription.push(
+            this.articleService.articles.subscribe(articles => {
+                this.articleList = articles;
+            })
+        );
+    }
+
+    ngOnDestroy() {
+        this.subscription.forEach(s => s.unsubscribe);
+    }
+
+    getArticleLink(article: Article): string {
+        return `/${routePaths.ARTICLE_READ.replace(':title', article.title.replace(/[\n\r\s]+/g, '_'))}_${article.id}`;
     }
 }
